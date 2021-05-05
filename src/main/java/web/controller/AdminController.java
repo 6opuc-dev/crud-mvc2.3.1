1e206aa3c452bf94dd90734d3113c64d65d5d316
6opuc-dev <75522440+6opuc-dev@users.noreply.github.com>
2021-05-05 21:53:36 +0300

    Update

---
+++ src/main/java/web/controller/AdminController.java
@@ -0,0 +1,67 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import web.hiber.service.UserService;
+import web.model.User;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public String getUsers(ModelMap model) {
+        model.addAttribute("users", userService.getUsers());
+        return "users";
+    }
+
+    @GetMapping("/new")
+    public String addUser(ModelMap model) {
+        model.addAttribute("user", new User());
+        return "new";
+    }
+
+    @PostMapping("/users")
+    public String createUser(@ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "new";
+        }
+        userService.addUser(user);
+        return "redirect:/admin/users";
+    }
+
+    @GetMapping("users/{id}")
+    public String getUserById(@PathVariable("id") String id, ModelMap model) {
+        model.addAttribute("user", userService.getUserById(id));
+        return "show";
+    }
+
+    @GetMapping("users/{id}/edit")
+    public String edit(ModelMap model, @PathVariable("id") String id) {
+        model.addAttribute("user", userService.getUserById(id));
+        return "edit";
+    }
+
+    @PatchMapping("users/{id}")
+    public String update(@ModelAttribute("user") @Valid User user, BindingResult bindingResult,
+                         @PathVariable("id") String id) {
+        if (bindingResult.hasErrors()) {
+            return "edit";
+        }
+        userService.updateUser(id, user);
+        return "redirect:/admin/users";
+    }
+
+    @DeleteMapping("users/{id}")
+    public String delete(@PathVariable("id") String id) {
+        userService.deleteUserById(id);
+        return "redirect:/admin/users";
+    }
+}
---
+++ src/main/java/web/model/Role.java
@@ -0,0 +1,61 @@
+package web.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role  implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String role;
+    @ManyToMany(mappedBy = "roles")
+    List<User> users;
+
+    public Role() {
+
+    }
+
+    public Role(String role) {
+        this.role = role;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String getAuthority() {
+        return getRole();
+    }
+
+    @Override
+    public String toString() {
+        return role="'" + role + "'";
+    }
+}
